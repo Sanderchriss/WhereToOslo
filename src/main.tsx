0bbca24270a9007949480c7dbdb9370a7094bbaf
Sanderchriss <sanderchriss@gmail.com>
2024-02-02 15:26:59 +0100

    Logo and publish is go

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<h1>Hello byyee</h1>);
+root.render(<h1>Her kommer kart</h1>);
